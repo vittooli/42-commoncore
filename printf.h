77ff9629428846ad05337987041fa6fc7e959ec7
Vittoria Oliviero <volivier@e3r5p3.42roma.it>
2024-02-01 11:26:55 +0100

    non dovrebbe servire

---
+++ printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volivier <volivier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 15:52:44 by volivier          #+#    #+#             */
+/*   Updated: 2024/01/27 00:44:30 by volivier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int nb);
+int ft_put_unsigned(unsigned int nb);
+int	ft_lowhex(unsigned long int nb);
+int	ft_uphex(unsigned long int nb);
+int	ft_pointer(unsigned long int pointer);
+
+#endif
