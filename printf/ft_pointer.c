0c9aa225bde9089a652d6c295e15fde6ef6d48c1
Vitto <vittoriaoliviero@MacBook-Air-di-Vitto.local>
2024-02-03 17:26:30 +0100

    makefile aggiustato

--- printf/ft_pointer.c
+++ printf/ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: volivier <volivier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 15:25:18 by volivier          #+#    #+#             */
-/*   Updated: 2024/02/01 10:49:26 by volivier         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:28:31 by volivier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ int	ft_len(unsigned long n)
 
 int	ft_pointer(void *pointer)
 {
+	if (pointer == NULL)
+		return (ft_putstr(NIL_STRING));
 	write(1, "0x", 2);
 	ft_lowhex((unsigned long int)(pointer));
 	return (ft_len((unsigned long int)(pointer)) + 2);
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: volivier <volivier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 10:51:01 by volivier          #+#    #+#             */
-/*   Updated: 2024/02/01 10:59:37 by volivier         ###   ########.fr       */
+/*   Updated: 2024/02/02 16:26:07 by volivier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+
+# ifdef __APPLE__
+#  define NIL_STRING "0x0"
+# endif
+# ifdef __unix__
+#  define NIL_STRING "(nil)"
+# endif
 
 int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
